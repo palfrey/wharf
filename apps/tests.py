a9a34ddeb7b3c323f854fe087b651f22b65e011f
Tom Parker-Shemilt <palfrey@tevp.net>
2019-02-01 22:29:57 +0000

    Add test to check is_safe_url

--- apps/tests.py
+++ apps/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import django
+django.setup()
 
-# Create your tests here.
+from django.test import TestCase
+import unittest
+import wharf.auth
+import types
+
+class AuthTests(unittest.TestCase):
+    def test_login_required_middleware(self):
+        mw = wharf.auth.LoginRequiredMiddleware(None)
+        request = types.SimpleNamespace()
+        request.path_info = "foo"
+        request.get_host = lambda: None
+        request.user = types.SimpleNamespace()
+        request.user.is_authenticated = False
+        mw(request)
