8aae0a2e61389e7de135e45695fd18ee8ea9bb7a
Filipe Souza <filipe.souza@weef.com.br>
2019-12-04 18:00:37 -0300

    - Started the template override with new form to add bootstrap 4 classes to fields;

--- apps/forms.py
+++ apps/forms.py
@@ -1,11 +1,25 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
+
 
 class ConfigForm(forms.Form):
     key = forms.CharField(label='key', max_length=100)
     value = forms.CharField(label='value', max_length=300)
 
+
 class CreateAppForm(forms.Form):
     name = forms.CharField(label='App name', max_length=100)
 
+
 class CreateDomainForm(forms.Form):
     name = forms.CharField(label='Domain name', max_length=100)
+
+
+class LoginForm(AuthenticationForm):
+
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 'class': 'form-control'}))
+    password = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+    )
