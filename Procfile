4d7a3576f315b0a1dfcf0db22aab18b3039cf26c
Filipe Souza <filipe.souza@weef.com.br>
2019-12-14 20:11:06 -0300

    - Initial commit with Weef version of Wharf;

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+release: python manage.py collectstatic --no-input
 web: bash dokku-boot.sh
 celery: python manage.py celery
--- wharf/urls.py
+++ wharf/urls.py
@@ -3,9 +3,12 @@ from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from apps.forms import LoginForm
 
 urlpatterns = [
     path('', include('apps.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
+    path('', include('social_django.urls', namespace='social')),
+    path('accounts/login/',
+         auth_views.LoginView.as_view(template_name="login.html", authentication_form=LoginForm), name='login'),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
