9d40cf148262e5fd3b73f7604640d7bf028ab3d5
Filipe Souza <filipe.souza@weef.com.br>
2020-03-23 17:09:20 -0300

    - Now there is a toogle visibility on global env variables values;

--- apps/static/assets/js/material-dashboard.js
+++ apps/static/assets/js/material-dashboard.js
@@ -732,3 +732,24 @@ function debounce(func, wait, immediate) {
     if (immediate && !timeout) func.apply(context, args);
   };
 };
+
+/* Wharf - Filipe Souza */
+var elements = document.getElementsByClassName("env-var-hidden-value-toogle");
+
+var toogle_hidden_value = function() {
+    var attribute = this.getAttribute("data-key-name");
+    var targetEl = document.getElementById('env-var-hidden-value-input-'+attribute);
+    if(targetEl.type === 'text')
+    {
+        targetEl.type = 'password';
+    }
+    else if(targetEl.type === 'password')
+    {
+        targetEl.type = 'text';
+    }
+    console.log(targetEl.type);
+};
+
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', toogle_hidden_value, false);
+}
