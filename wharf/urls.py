f988c558cda3cd89fa9fa734b0c6280443b7e448
Tom Parker-Shemilt <palfrey@tevp.net>
2020-04-22 22:58:40 +0100

    Do staticfiles serving even in prod

--- wharf/urls.py
+++ wharf/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
-from django.conf.urls.static import static
-from apps.forms import LoginForm
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('apps.urls')),
@@ -11,4 +10,5 @@ urlpatterns = [
     path('accounts/login/',
          auth_views.LoginView.as_view(template_name="login.html"), name='login'),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
