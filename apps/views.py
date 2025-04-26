36a9afeecb96db3a9924bf34a538a57c458d7825
Tom Parker-Shemilt <palfrey@tevp.net>
2025-04-26 18:45:56 +0100

    Fix check_app regex

--- apps/views.py
+++ apps/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib import messages
+from django.urls import reverse
 import wharf.tasks as tasks
 from celery.result import AsyncResult
 from celery.states import state, PENDING, SUCCESS, FAILURE, STARTED
 from django.core.cache import cache
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponseBadRequest, HttpResponse, HttpResponseServerError
+from django.http import HttpRequest, HttpResponseBadRequest, HttpResponse, HttpResponseServerError
 
-import requests
-import time
 from . import forms
 from . import models
 import re
@@ -57,7 +56,7 @@ def run_cmd_with_log(app_name, description, cmd, after):
         ).save()
     return redirect(reverse('wait_for_command', kwargs={'app_name': app_name, 'task_id': res.id, 'after': after}))
 
-def get_log(res):
+def get_log(res: AsyncResult):
     key = tasks.task_key(res.id)
     if res.state > state(PENDING):
         raw = redis.get(key)
@@ -67,7 +66,7 @@ def get_log(res):
     else:
         return ""
 
-def wait_for_command(request, app_name, task_id, after):
+def wait_for_command(request: HttpRequest, app_name, task_id, after):
     res = AsyncResult(task_id)
     if app_name != '_':
         app = models.App.objects.get(name=app_name)
@@ -89,7 +88,7 @@ def wait_for_command(request, app_name, task_id, after):
         'description': description
         })
 
-def show_log(request, task_id):
+def show_log(request: HttpRequest, task_id: str):
     res = AsyncResult(task_id)
     task = models.TaskLog.objects.get(task_id=task_id)
     log = ansi_escape.sub("", get_log(res))
@@ -110,7 +109,7 @@ def app_list():
         raise Exception(data)
     return lines[1:]
 
-def index(request):
+def index(request: HttpRequest):
     try:
         apps = app_list()
     except Exception as e:
@@ -128,7 +127,7 @@ def index(request):
     config = global_config()
     return render(request, 'list_apps.html', {'apps': apps, 'app_form': app_form, 'config_form': config_form, 'config': sorted(config.items())})
 
-def refresh_all(request):
+def refresh_all(request: HttpRequest):
     cache.clear()
     return redirect(reverse('index'))
 
@@ -153,7 +152,7 @@ def global_config():
 def app_config_set(app, key, value):
     return run_cmd_with_log(app, "Setting %s" % key, "config:set %s %s=%s" % (app, key, value), "check_app_config_set")
 
-def check_config_set(request, task_id):
+def check_config_set(request: HttpRequest, task_id: str):
     res = AsyncResult(task_id)
     data = get_log(res)
     lines = data.split("\n")
@@ -161,7 +160,7 @@ def check_config_set(request, task_id):
         raise Exception(data)
     messages.success(request, 'Config updated')
 
-def check_app_config_set(request, app_name, task_id):
+def check_app_config_set(request: HttpRequest, app_name, task_id: str):
     check_config_set(request, task_id)
     clear_cache("config %s" % app_name)
     return redirect(reverse('app_info', args=[app_name]))
@@ -173,19 +172,19 @@ def global_config_set(request):
     else:
         raise Exception
 
-def check_global_config_set(request, task_id):
+def check_global_config_set(request: HttpRequest, task_id: str):
     check_config_set(request, task_id)
     clear_cache("config --global")
     return redirect(reverse('index'))
 
-def generic_list(app_name, data, name_field, fields):
+def generic_list(app_name, data, name_field: str, field_names: list[str]):
     lines = data.split("\n")
     if lines[0].find("is not a dokku command") != -1:
         raise Exception("Need plugin!")
     if lines[0].find("There are no") != -1:
         return None
-    fields = dict([[x,{}] for x in fields])
-    last_field = None
+    fields = dict([(x,{}) for x in field_names])
+    last_field: str | None = None
     for f in fields.keys():
         index = lines[0].find(f)
         if index == -1:
@@ -193,9 +192,10 @@ def generic_list(app_name, data, name_field, fields):
         if f == name_field:
             index = 0
         fields[f]["start"] = index
-        if last_field != None:
+        if last_field is not None:
             fields[last_field]["end"] = index
         last_field = f
+    assert last_field is not None
     fields[last_field]["end"] = None
     results = []
     for line in lines[1:]:
@@ -212,10 +212,10 @@ def generic_list(app_name, data, name_field, fields):
     else:
         return None
 
-def db_list(app_name, data):
+def db_list(app_name: str, data):
     return generic_list(app_name, data, "NAME", ["NAME", "VERSION", "STATUS", "EXPOSED PORTS", "LINKS"])
 
-def postgres_list(app_name):
+def postgres_list(app_name: str):
     data = run_cmd_with_cache("postgres:list")
     try:
         return db_list(app_name, data)
@@ -231,7 +231,7 @@ def redis_list(app_name):
         clear_cache("redis:list")
         raise
 
-def letsencrypt(app_name):
+def letsencrypt(app_name: str):
     data = run_cmd_with_cache("letsencrypt:ls")
     return generic_list(app_name, data, "App name", ["App name", "Certificate Expiry", "Time before expiry", "Time before renewal"])
 
@@ -258,12 +258,13 @@ def process_info(app_name):
     results["processes"] = processes
     return results
 
-def domains_list(app_name):
+def domains_list(app_name: str) -> list[str]:
     data = run_cmd_with_cache("domains:report %s" % app_name)
     vhosts = re.search("Domains app vhosts: (.*)", data)
+    assert vhosts is not None
     return [x.strip() for x in vhosts.groups()[0].split(" ") if x != ""]
 
-def add_domain(request, app_name):
+def add_domain(request: HttpRequest, app_name: str):
     form = forms.CreateDomainForm(request.POST)
     if form.is_valid():
         commands = ["domains:add %s %s" % (app_name, form.cleaned_data['name'])]
@@ -273,7 +274,7 @@ def add_domain(request, app_name):
     else:
         raise Exception
 
-def check_domain(request, app_name, task_id):
+def check_domain(request: HttpRequest, app_name, task_id: str):
     res = AsyncResult(task_id)
     data = get_log(res)
     if data.find("Reloading nginx") != -1:
@@ -283,14 +284,14 @@ def check_domain(request, app_name, task_id):
     else:
         raise Exception(data)
 
-def remove_domain(request, app_name):
+def remove_domain(request: HttpRequest, app_name):
     name = request.POST['name']
     commands = ["domains:remove %s %s" % (app_name, name)]
     if letsencrypt(app_name) != None:
         commands.append("letsencrypt %s" % app_name)
     return run_cmd_with_log(app_name, "Remove domain %s" % name, commands, "check_domain")
 
-def app_info(request, app_name):
+def app_info(request: HttpRequest, app_name):
     app, _ = models.App.objects.get_or_create(name=app_name)
     config = app_config(app_name)
     if "GITHUB_URL" in config:
@@ -317,7 +318,7 @@ def app_info(request, app_name):
         'task_logs': models.TaskLog.objects.filter(app=app).order_by('-when').all(),
     })
 
-def deploy(request, app_name):
+def deploy(request: HttpRequest, app_name):
     if request.POST['action'] == "deploy":
         res = tasks.deploy.delay(app_name, request.POST['url'])
         clear_cache("config %s" % app_name)
@@ -329,18 +330,18 @@ def deploy(request, app_name):
     else:
         raise Exception(request.POST['action'])
 
-def create_postgres(request, app_name):
+def create_postgres(request: HttpRequest, app_name):
     return run_cmd_with_log(app_name, "Add Postgres", ["postgres:create %s" % app_name, "postgres:link %s %s" % (app_name, app_name)], "check_postgres")
 
-def create_redis(request, app_name):
+def create_redis(request: HttpRequest, app_name):
     return run_cmd_with_log(app_name, "Add Redis", ["redis:create %s" % app_name, "redis:link %s %s" % (app_name, app_name)], "check_redis")
 
-def check_deploy(request, app_name, task_id):
+def check_deploy(request: HttpRequest, app_name, task_id: str):
     clear_cache("config %s" % app_name)
     messages.success(request, "%s redeployed" % app_name)
     return redirect(reverse('app_info', args=[app_name]))
 
-def check_rebuild(request, app_name, task_id):
+def check_rebuild(request: HttpRequest, app_name, task_id: str):
     res = AsyncResult(task_id)
     data = get_log(res)
     if data.find("Application deployed:") == -1:
@@ -349,7 +350,7 @@ def check_rebuild(request, app_name, task_id):
     clear_cache("config %s" % app_name)
     return redirect(reverse('app_info', args=[app_name]))
 
-def check_postgres(request, app_name, task_id):
+def check_postgres(request: HttpRequest, app_name, task_id: str):
     res = AsyncResult(task_id)
     data = get_log(res)
     if data.find("Postgres container created") == -1:
@@ -359,7 +360,7 @@ def check_postgres(request, app_name, task_id):
     clear_cache("config %s" % app_name)
     return redirect(reverse('app_info', args=[app_name]))
 
-def check_redis(request, app_name, task_id):
+def check_redis(request: HttpRequest, app_name, task_id: str):
     res = AsyncResult(task_id)
     data = get_log(res)
     if data.find("Redis container created") == -1:
@@ -369,23 +370,23 @@ def check_redis(request, app_name, task_id):
     clear_cache("config %s" % app_name)
     return redirect(reverse('app_info', args=[app_name]))
 
-def create_app(app_name):
+def create_app(app_name: str):
     models.App(name=app_name).save()
     return run_cmd_with_log(app_name, "Add app %s" % app_name, "apps:create %s" % app_name, "check_app")
 
-def check_app(request, app_name, task_id):
+def check_app(request: HttpRequest, app_name: str, task_id: str):
     res = AsyncResult(task_id)
     data = get_log(res)
-    if data.find("Creating %s... done" % app_name) == -1:
+    if data.find("Creating %s..." % app_name) == -1:
         raise Exception(data)
     messages.success(request, "Created %s" % app_name)
     clear_cache("apps:list")
     return redirect(reverse('app_info', args=[app_name]))
 
-def setup_letsencrypt(request, app_name):
+def setup_letsencrypt(request: HttpRequest, app_name: str):
     return run_cmd_with_log(app_name, "Enable Let's Encrypt", "letsencrypt %s" % app_name, "check_letsencrypt")
 
-def check_letsencrypt(request, app_name, task_id):
+def check_letsencrypt(request: HttpRequest, app_name: str, task_id: str):
     res = AsyncResult(task_id)
     log = get_log(res)
     if log.find("Certificate retrieved successfully") !=-1:
@@ -395,7 +396,7 @@ def check_letsencrypt(request, app_name, task_id):
         return render(request, 'command_wait.html', {'app': app_name, 'task_id': task_id, 'log': log, 'state': res.state, 'running': res.state in [state(PENDING), state(STARTED)]})
 
 @csrf_exempt
-def github_webhook(request):
+def github_webhook(request: HttpRequest):
     secret = settings.GITHUB_SECRET.encode('utf-8')
     hash = "sha1=%s" % hmac.new(secret, request.body, hashlib.sha1).hexdigest()
     if "HTTP_X_HUB_SIGNATURE" not in request.META:
@@ -416,6 +417,7 @@ def github_webhook(request):
     if not apps.exists():
         return HttpResponseBadRequest("Can't find an entry for clone URL %s" % clone_url)
     app = apps.first()
+    assert app is not None
     res = tasks.deploy.delay(app.name, clone_url)
     clear_cache("config %s" % app.name)
     return HttpResponse("Running deploy. Deploy log is at %s" % request.build_absolute_uri(reverse('show_log', kwargs={'task_id': res.id})))
@@ -429,7 +431,7 @@ def check_status():
     clear_cache("config --global")
     run_cmd_with_cache("config --global")
 
-def status(request):
+def status(request: HttpRequest):
     try:
         check_status()
         return HttpResponse("All good")
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,5 +1,11 @@
 from unittest.mock import MagicMock, patch
-from apps.views import app_list
+
+from django.http import HttpRequest
+import pytest
+from apps.views import app_list, check_app
+from celery.result import AsyncResult
+from celery.states import state, PENDING, SUCCESS, FAILURE, STARTED
+from redis import StrictRedis
 
 class MockCelery:
     def __init__(self, res: str):
@@ -19,7 +25,25 @@ def mock_commands(*args):
     print(args)
     raise Exception
 
+@pytest.fixture
+def mock_request() -> HttpRequest:
+    mr = MagicMock(spec=HttpRequest)
+    mr.META = MagicMock()
+    mr._messages = MagicMock()
+    return mr
+
 @patch("wharf.tasks.run_ssh_command.delay")
 def test_app_list(patched_delay: MagicMock):
     patched_delay.side_effect = mock_commands
     assert app_list() == ["wharf"]
+
+@patch("wharf.tasks.run_ssh_command.delay")
+def test_check_app(patched_delay: MagicMock, mock_request: HttpRequest, monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(AsyncResult, "state", state(SUCCESS))
+    monkeypatch.setattr(StrictRedis, "get", lambda _self, _key: b"""Creating 3b56c612721a4b88a6fece5d67aa90a3...
+-----> Creating new app virtual host file...""")
+
+    patched_delay.side_effect = mock_commands
+    resp = check_app(mock_request, "3b56c612721a4b88a6fece5d67aa90a3", "1234")
+    assert resp.status_code == 302, resp
+    assert resp.url == "/apps/3b56c612721a4b88a6fece5d67aa90a3"
